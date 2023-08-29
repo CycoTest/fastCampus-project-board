408c52ad982f45df09ca6cfb059fecfc8f969aa0
CycoTest <aceldamadk@gmail.com>
2023-08-29 18:29:06 +0900

    #6 - 스프링 부트 프로젝트 시작하기
    스프링 부트 프로젝트 초기화
    
    들어간 기술
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ FirstCampus-project-board/src/test/java/com/firstcampus/projectboard/FirstCampusProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstcampus.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCampusProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
